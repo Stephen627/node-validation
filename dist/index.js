8a291c730125eafb90b5a4103f5f6fbc7f85b5a3
Stephen James <spjames@gmail.com>
2018-04-02 16:13:48 +0100

    removing dist and node_modules from git

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const validator_1 = require("./validator");
-exports.Validator = validator_1.default;
-const validationError_1 = require("./validationError");
-exports.ValidationError = validationError_1.default;
-//# sourceMappingURL=index.js.map
--- dist/validationError.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class ValidationError extends Error {
-}
-exports.default = ValidationError;
-//# sourceMappingURL=validationError.js.map

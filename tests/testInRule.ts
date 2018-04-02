3bf76ae2a42b3c9a9ec7d8a5bcea0c6727892aee
Stephen James <spjames@gmail.com>
2018-04-02 18:03:23 +0100

    added test for in rule

---
+++ tests/testInRule.ts
@@ -0,0 +1,46 @@
+import mocha from 'mocha'
+import { expect } from 'chai'
+import Validator from '../validator'
+
+describe('In Rule', () => {
+  it('should pass if the rule is present and data is not', () => {
+    let validator: Validator = new Validator()
+
+    validator.setRules({
+      'test': 'in:foo,bar'
+    }).setData({
+    })
+
+    validator.passes()
+
+    expect(validator.getErrors()).to.be.length(0)
+  })
+
+  it('should fail if the data does not match the rule provided', () => {
+    let validator: Validator = new Validator()
+
+    validator.setRules({
+      'test': 'in:foo,bar'
+    }).setData({
+      'test': 'abc' 
+    })
+
+    validator.passes()
+
+    expect(validator.getErrors()).to.be.length(1)
+  })
+
+  it('should pass if the data matches to the rule provided', () => {
+    let validator: Validator = new Validator()
+
+    validator.setRules({
+      'test': 'in:foo,bar'
+    }).setData({
+      'test': 'foo'
+    })
+
+    validator.passes()
+
+    expect(validator.getErrors()).to.be.length(0)
+  })
+})
